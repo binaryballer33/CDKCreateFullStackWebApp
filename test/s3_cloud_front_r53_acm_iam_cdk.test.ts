1cad31ce2c8ed9a2840a55248eab78341fbcf8fd
shaqmand <shaqmand@amazon.com>
2023-03-06 19:21:08 -0800

    Initial commit

---
+++ test/s3_cloud_front_r53_acm_iam_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3CloudFrontR53AcmIamCdk from '../lib/s3_cloud_front_r53_acm_iam_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3_cloud_front_r53_acm_iam_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3CloudFrontR53AcmIamCdk.S3CloudFrontR53AcmIamCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
